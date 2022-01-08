a8cd5186e7304fd684fbe192c9dec08392e8dd5c
MagicalShark <59572170+MagicalShark@users.noreply.github.com>
2022-01-08 17:29:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+//% color=190 weight=100 icon="\f558" block="Geekid Data Blocks" advanced=true
+namespace DataEx {
+    //% blockId=saveDataToCloud
+    //% block = "将变量名为$key值为$value存储到云端数据库"
+    export function saveDataToCloud(key: string, value: string): void {
+        
+    }
+}
